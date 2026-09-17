0cb5cb2fee7163da219d9e56af634b395649b8ae
Alessandro Zovico <zovico.dev@gmail.com>
2026-09-17 13:13:38 -0300

    Delete EcoGiro 2.1 directory

--- EcoGiro 2.1/src/main/java/com/ecogiro/backend/repository/RecomendacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ecogiro.backend.repository;
-
-import com.ecogiro.backend.entity.RecomendacaoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecomendacaoRepository extends JpaRepository<RecomendacaoEntity, Long> {
-}
