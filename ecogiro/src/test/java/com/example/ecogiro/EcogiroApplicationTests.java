77d6857a0f19d3c79d4e8409ef767551975bc45d
Pedro Henrique Ferreira Gomes <phfgomes.14@gmail.com>
2026-09-23 14:59:22 -0300

    Add files via upload

---
+++ ecogiro/src/test/java/com/example/ecogiro/EcogiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecogiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcogiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
